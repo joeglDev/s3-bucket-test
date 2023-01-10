824fea1b77688a9b8a000ce8ecc7ac6a866360c0
Hiroji <joegilbertdev@outlook.com>
2023-01-10 19:18:00 +0000

    sample.ts: upload test img to s3 bucket

--- src/API/sample.ts
+++ src/API/sample.ts
@@ -29,5 +29,27 @@ const params = {
       }
   }
 
+  /**
+   * 
+   * @returns 
+   */
+  const uploadObject = async () => {
+    // Create an object and upload it to the Amazon S3 bucket.
+  try {
+    const results = await s3Client.send(new PutObjectCommand(params));
+    console.log(
+        "Successfully created " +
+        params.Key +
+        " and uploaded it to " +
+        params.Bucket +
+        "/" +
+        params.Key
+    );
+    return results; // For unit tests.
+  } catch (err) {
+    console.log("Error", err);
+  }
+  }
 
-  export {createBucket};
+
+  export {createBucket, uploadObject};
